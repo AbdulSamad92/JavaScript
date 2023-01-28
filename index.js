edab019c4b1bb28f867458feb5d158d277c18398
AbdulSamad92 <121524190+AbdulSamad92@users.noreply.github.com>
2023-01-28 23:07:06 +0300

    java

---
+++ index.js
@@ -0,0 +1,9 @@
+var percentage = prompt ("Enter your percentage")
+function percentage () {
+    if(percentage < 40){'Fail'}
+    else if(percentage >= 40 && percentage < 50){'D'}
+    else if(percentage >= 50 && percentage < 60){'C'}
+    else if(percentage >= 60 && percentage < 70){'B'}
+    else if(percentage >= 70 && percentage < 80){'A'}
+    else(percentage >= 80){'A+'}
+}
