713c84d47df2689bca8f3d6541b8373eb5b7e5f3
AbdulSamad92 <121524190+AbdulSamad92@users.noreply.github.com>
2023-01-29 02:13:19 +0300

    java

--- index.js
+++ index.js
@@ -1,10 +1,18 @@
 var percentage = prompt ("Enter your percentage")
-function percent () {
-    if(percentage < 40){'Fail'}
-    else if(percentage >= 40 && percentage < 50){'D'}
-    else if(percentage >= 50 && percentage < 60){'C'}
-    else if(percentage >= 60 && percentage < 70){'B'}
-    else if(percentage >= 70 && percentage < 80){'A'}
-    else(percentage >= 80){'A+'}
+function percent (percentage) {
+    if(percentage >= 80){alert('A+')}
+    else if(percentage >= 70 && percentage < 80){alert('A')}
+    else if(percentage >= 60 && percentage < 70){alert('B')}
+    else if(percentage >= 50 && percentage < 60){alert('C')}
+    else if(percentage >= 40 && percentage < 50){alert('D')}
+    else(alert('Fail')
 }
-alert (percentage)
+
+
+
+
+
+
+
+
+
